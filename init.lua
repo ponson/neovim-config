89741329e6e5d45a88e388997c578f91d92586d5
Ponson Sun <ponsonsun@gmail.com>
2024-07-21 20:52:23 +0800

    second commit

--- init.lua
+++ init.lua
@@ -11,6 +11,7 @@ vim.api.nvim_create_autocmd({ "TextYankPost" }, {
 		})
 	end,
 })
+
 -- keybindings
 local opt = { noremap = true, silent = true }
 vim.g.mapleader = " "
@@ -23,3 +24,24 @@ vim.keymap.set("n", "<Leader>s", "<C-w>s", opt)
 -- https://www.reddit.com/r/vim/comments/2k4cbr/problem_with_gj_and_gk/
 vim.keymap.set("n", "j", [[v:count ? 'j' : 'gj']], { noremap = true, expr = true })
 vim.keymap.set("n", "k", [[v:count ? 'k' : 'gk']], { noremap = true, expr = true })
+
+-- lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup({
+	{
+		"RRethy/nvim-base16",
+		lazy = true,
+	},
+})
+vim.cmd.colorscheme("base16-tender")
